9df84c7287a52b72edb438008617e5776bfaaa68
katevi <kate.vinnik393@gmail.com>
2023-03-18 18:28:20 +0300

    Add echo response

---
+++ go.mod
@@ -0,0 +1,8 @@
+module goroscope.bot
+
+go 1.20
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+const (
+	TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
+)
+
+func getToken(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(getToken(TELEGRAM_BOT_TOKEN))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			log.Printf("An update was received: %s", update.Message)
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
