7788f61a3c8cc17f6b2e0b17b16baadfb557285c
katevi <kate.vinnik393@gmail.com>
2023-03-23 23:09:49 +0300

    Add simple goroscope generation

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -38,9 +40,63 @@ func main() {
 			log.Printf("A message %s was received from %v", update.Message.Text, update.Message.From)
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, generateHoroscope())
 		msg.ReplyToMessageID = update.Message.MessageID
 
 		bot.Send(msg)
 	}
+}
+
+// Goroscope generation
+
+var (
+	creatures = []string{"Red Dragon", "Black dragon", "Peasant",
+		"Goblin", "Dwarf", "Orc", "Gargoyle", "Ghost", "Genie", "Medusa",
+		"Halfling", "Boar", "Iron Golem", "Roc", "Orc", "Wolf", "Ogre", "Troll", "Cyclop",
+		"Unicorn", "Phoenix", "Sprite", "Dwarf"}
+	plagueWeek        = "Plague"
+	weekOf            = append(creatures, plagueWeek)
+	necromances       = []string{"Zombie", "Skeleton", "Mummy", "Vampire", "Lich"}
+	dwellingsSentence = "All dwellings increase population."
+)
+
+func generateHoroscope() string {
+	fst, seed := generateFirstSentence()
+	snd := generateSecondSentence(seed)
+	return fmt.Sprint(fst, snd)
+}
+
+func generateFirstSentence() (sentence, seed string) {
+	weekOf := pickRandomElement(weekOf)
+	return fmt.Sprintf("Astrologers proclaim the week of %s. ", weekOf), weekOf
+}
+
+func generateSecondSentence(seed string) string {
+	if seed == plagueWeek {
+		return generateNecromancesIncreased()
+	}
+	return generateCreaturesIncreased(seed)
+}
+
+func generateNecromancesIncreased() string {
+	return fmt.Sprintf("%s growth +%d. ", pickRandomElement(necromances), rand.Intn(10))
+}
+
+func generateCreaturesIncreased(seed string) string {
+	creaturesIncreased := fmt.Sprintf("%s growth +%d. ", seed, rand.Intn(10))
+	if randBool() {
+		return fmt.Sprint(creaturesIncreased, dwellingsSentence)
+	}
+	return creaturesIncreased
+}
+
+// Helper functions for generation
+
+func randBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func pickRandomElement(array []string) string {
+	randomIndex := rand.Intn(len(array))
+	return array[randomIndex]
 }
